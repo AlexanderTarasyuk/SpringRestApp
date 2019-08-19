b0082df57682d9e87e792e4870a1d3d1da8abae0
alex <vergolff@gmail.comcd>
2019-08-19 18:19:09 +0300

    enable retry is implemented

--- src/main/java/hillel/spring/Application.java
+++ src/main/java/hillel/spring/Application.java
@@ -2,9 +2,10 @@ package hillel.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
-//@EnableRetry
+@EnableRetry
 public class Application {
 
     public static void main(String[] args) {
